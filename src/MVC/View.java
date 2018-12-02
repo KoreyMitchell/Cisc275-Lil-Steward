8807f20bb4d770098bf32e3cbf0bff92229c2ac2
RodneyNickles <36527686+RodneyNickles@users.noreply.github.com>
2018-12-02 16:58:54 -0500

    Update View.java

--- src/MVC/View.java
+++ src/MVC/View.java
@@ -213,7 +213,7 @@ public class View extends JPanel implements MouseListener, KeyListener{
 		
 		}else if(State ==STATE.MENU) {//if game state is not in game,draw menu
 			g2d.drawImage(scaled_bg_img_menu, 0, 0, null);
-			menu.render(g);
+			menu.renderMenu(g);
 		
 		}else if(State == STATE.END) {	//if game is ended
 			//g2d.drawImage(backgroundimg, 0, 0, null);
