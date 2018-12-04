5c4cbdc3db0f1509e72fa38852853dc219434e16
Toby Boyd <eboyd@udel.edu>
2018-12-04 18:19:54 -0500

    Changed endscreen

--- src/MVC/EndScreen.java
+++ src/MVC/EndScreen.java
@@ -19,7 +19,7 @@ public class EndScreen extends View{
 		Font fnt0 = new Font("arial",Font.BOLD,50); //font,bold,size		
 		g.setFont(fnt0);		
 		g.setColor(Color.RED);
-		g.drawString("Game Over!",screenWidth/2-150,100 );
+		g.drawString("Congratulations!",screenWidth/2-150,100 );
 		g.drawString("You planted " + plantedCount + " native plants!", screenWidth/2-150,300);
 		g.drawString("You removed " + plantsRemoved + " invasive plants!", screenWidth/2-150,500);
 		
