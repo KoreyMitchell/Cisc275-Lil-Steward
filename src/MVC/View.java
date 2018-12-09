75eb6c597d5aafb08d7403b21ffde01488968f44
Matt Lee <mattlee@udel.edu>
2018-12-09 15:23:10 -0500

    c3

--- src/MVC/View.java
+++ src/MVC/View.java
@@ -362,6 +362,8 @@ public class View extends JPanel implements MouseListener, KeyListener{
 
 	/** The i. */
 	protected int i;
+
+	private Color mixed;
 	
 	/* (non-Javadoc)
 	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
@@ -375,20 +377,15 @@ public class View extends JPanel implements MouseListener, KeyListener{
 		//if in GAME state
 		if(State == STATE.GAME) {
 			if(level == 0) {
-				
-				//g2d.drawImage(scaled_bg_img, 0, 0, null);//draw background
-				g2d.drawImage(playerimg, player.getXloc(), player.getYloc(), null);//draw player 
 
-				
-				g2d.setFont(new Font("Dialog", Font.BOLD,30));
-				g2d.drawRect(0, 0, 450, 140);
+				//Draw time graphics
+
+				g2d.drawImage(scaled_bg_img, 0, 0, null);
+				g2d.setFont(new Font("Dialog", Font.BOLD,25));
 				g2d.setColor(Color.white);
-				g2d.fillRect(0, 0, 450, 140);
-				g2d.setColor(Color.BLACK);
-
-				g2d.drawString("White Wood Asters planted: " + plantedCount,5, 30);
-				g2d.drawString("Phragmites Removed: " + plantsRemoved ,5, 70);
-				g2d.drawString("Time: " + secondsPassed ,5, 110);	
+				g2d.drawString("White Wood Asters planted: " + plantedCount + "  |   Phragmites Removed: " + plantsRemoved + 
+						"  |   Time: " + secondsPassed, screenWidth/2-400, 30);
+					
 				
 				// draw each of the game objects
 				for (GroundPatch gr : groundList) {
